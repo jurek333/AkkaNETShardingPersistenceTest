c30adab586a45ceddbc488e1cf138a87d66e885b
Maciek <jurek.333@gmail.com>
2016-02-18 13:37:39 +0100

    project with 3 sets of persistence configurations to play with.

---
+++ AkkaNET.Sharding.TwoConfigurationsForPersistence/Program.cs
@@ -0,0 +1,65 @@
+﻿using Akka.Actor;
+using Akka.Cluster.Sharding;
+using Akka.Persistence.Sqlite;
+using Akka.Persistence.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaNET.Sharding.TwoConfigurationsForPersistence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActorSystem system = ActorSystem.Create("AkkaCluster");
+            SqlitePersistence.Get(system);
+            SqlServerPersistence.Get(system);
+
+            IActorRef region = ClusterSharding.Get(system).Start(
+                typeName: typeof(Worker).Name,
+                entityProps: Props.Create<Worker>(),
+                settings: ClusterShardingSettings.Create(system),
+                messageExtractor: new Worker.Message.Extractor()
+                );
+
+            IActorRef bell = system.ActorOf(Props.Create<Bell>(region));
+            Bell.RingTheBell ringTheBell = new Bell.RingTheBell();
+
+            system.Scheduler.ScheduleTellRepeatedly(
+                initialDelay: TimeSpan.FromSeconds(10),
+                interval: TimeSpan.FromSeconds(1),
+                receiver: bell,
+                sender: ActorRefs.Nobody,
+                message: ringTheBell
+                );
+
+            Console.WriteLine("App is working. Press any key to exit...");
+            Console.ReadKey();
+
+            Task.WaitAll(system.Terminate());
+
+            Console.WriteLine("Exiting...");
+        }
+    }
+
+    public class Bell: ReceiveActor
+    {
+        private IActorRef _shardRegion;
+        private int _innerCounter;
+        public Bell(IActorRef shardRegion)
+        {
+            _shardRegion = shardRegion;
+
+            Receive<RingTheBell>(msg =>
+            {
+                int id = ++_innerCounter % 10;
+                _shardRegion.Tell(new Worker.Message(id, _innerCounter));
+            });
+        }
+
+        public class RingTheBell { }
+    }
+}
---
+++ AkkaNET.Sharding.TwoConfigurationsForPersistence/Worker.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Akka.Cluster.Sharding;
+using Akka.Persistence;
+using Akka.Actor;
+
+namespace AkkaNET.Sharding.TwoConfigurationsForPersistence
+{
+    public class Worker: ReceivePersistentActor
+    {
+        private const int _timeoutInSeconds = 60;
+        public override string PersistenceId { get { return Self.Path.Name; } }
+
+        private WorkerContent _content;
+
+        public Worker() {
+            _content = new WorkerContent();
+
+            Recover<SnapshotOffer>(offer =>
+            {
+                var newContent = offer.Snapshot as WorkerContent;
+                if (newContent != null)
+                {
+                    _content = newContent;
+                }
+            });
+            Command<SaveSnapshotSuccess>(success => DeleteMessages(success.Metadata.SequenceNr, false));
+            Command<SaveSnapshotFailure>(failure => { /* do nothing */ });
+            Recover<WorkerEvent>(ev => Handle(ev));
+            Command<Message>(msg => {
+                WorkerEvent ev = new WorkerEvent(msg);
+                Persist(ev, e => Handle(e));
+            });
+
+            Command<ReceiveTimeout>(msg =>
+            {
+                Context.Parent.Tell(new Passivate(new StopActor()));
+            });
+            Command<StopActor>(msg =>
+            {
+                Context.Stop(Self);
+            });
+            Context.SetReceiveTimeout(TimeSpan.FromSeconds(_timeoutInSeconds));
+        }
+
+        private void Handle(WorkerEvent ev)
+        {
+            _content = _content.Handle(ev);
+            if (_content.TotalCountOfEvents % 3 == 0)
+            {
+                SaveSnapshot(_content);
+            }
+        }
+
+        public class WorkerEvent
+        {
+            public DateTime WhenCreated { get; set; }
+            public int MsgNumber { get; set; }
+
+            public WorkerEvent() {
+                WhenCreated = DateTime.UtcNow;
+            }
+            public WorkerEvent(Message msg)
+            {
+                WhenCreated = msg.WhenCreated;
+                MsgNumber = msg.MsgNumber;
+            }
+        }
+
+        public class StopActor {
+            public DateTime WhenCreated { get; }
+
+            public StopActor()
+            {
+                WhenCreated = DateTime.UtcNow;
+            }
+        }
+        public class Message
+        {
+            public int Id { get; private set; }
+            public DateTime WhenCreated { get; private set; }
+            public int MsgNumber { get; private set; }
+
+            public Message(int id) {
+                WhenCreated = DateTime.UtcNow;
+                Id = id;
+            }
+
+            public Message(int id, int num):this(id)
+            {
+                MsgNumber = num;
+            }
+
+            public class Extractor : IMessageExtractor
+            {
+                public string EntityId(object message)
+                {
+                    return (message as Worker.Message)?.Id.ToString();
+                }
+
+                public object EntityMessage(object message)
+                {
+                    return message;
+                }
+
+                public string ShardId(object message)
+                {
+                    return ((message as Worker.Message)?.Id % 10).ToString();
+                }
+            }
+        }
+    }
+
+    public class WorkerContent
+    {
+        public int LastNumber { get; }
+        public DateTime TimeOfLastEvent { get; }
+        public int TotalCountOfEvents { get; }
+
+        public WorkerContent()
+        {
+            TimeOfLastEvent = DateTime.UtcNow;
+        }
+
+        public WorkerContent(int num, int total):this() {
+            LastNumber = num;
+            TotalCountOfEvents = total;
+        }
+
+        public WorkerContent Handle(Worker.WorkerEvent ev)
+        {
+            return new WorkerContent(ev.MsgNumber, 1 + this.TotalCountOfEvents);
+        }
+    }
+}
